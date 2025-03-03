7f5f1c108b3aa1d4e89a5a88d83dd40627c49890
Igor Polousov <igorpolousov@gmail.com>
2025-03-03 15:23:02 +0300

    Initial Commit

---
+++ RemindersList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RemindersList
+//
+//  Created by Igor Polousov on 03.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
